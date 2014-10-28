14967204358f1df65bdbc4f110b57f5a72287739
Justin Caratzas <bigjust@lambdaphil.es>
2014-10-28 13:36:30 -0400

    [DB-153] use import_recalls for import task

--- loop/loop/apps/recalls/tests/test_import_command.py
+++ loop/loop/apps/recalls/tests/test_import_command.py
@@ -1,3 +1,57 @@
+import datetime
+import os
+import responses
+
+from django.core.management import call_command
 from django.test import TestCase
 
-# some tests for the management command, for when we add args to the command
+from recalls.api_client import recall_api
+
+
+class TestImportRecallCommand(TestCase):
+
+    def setUp(self):
+        self.api_client = recall_api()
+
+    def stub_import_responses(self):
+        responses.add(
+            responses.GET,
+            '{url}'.format(url=self.api_client.base_url),
+            body=open(os.path.join(os.path.dirname(__file__),
+                                   'testdata/response_pg1.json'), 'r').read(),
+            status=200,
+            content_type='application/json'
+        )
+        responses.add(
+            responses.GET,
+            '{url}'.format(url=self.api_client.base_url),
+            body=open(os.path.join(os.path.dirname(__file__),
+                                   'testdata/response_pg2.json'), 'r').read(),
+            status=200,
+            content_type='application/json'
+        )
+
+
+    @responses.activate
+    def test_no_args(self):
+        """
+        Test that the command tries to import latest recalls.
+        """
+        self.stub_import_responses()
+        call_command('import_recalls')
+
+        import ipdb; ipdb.set_trace()
+        self.assertEqual(len(responses.calls), 2)
+
+    def test_date_args(self):
+        """
+        Test that the command passes start_date and end_date params.
+        """
+        pass
+
+    def test_since_latest_recall(self):
+        """
+        Test that the import command will try to import all recalls since
+        the last one in the database.
+        """
+        pass
