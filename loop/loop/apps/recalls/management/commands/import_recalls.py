3ff3cbb4738df042248f503ac5a7ff33030e0a63
Justin Caratzas <bigjust@lambdaphil.es>
2014-10-28 11:12:44 -0400

    [SB-153] implement task to import recalls

--- loop/loop/apps/recalls/management/commands/import_recalls.py
+++ loop/loop/apps/recalls/management/commands/import_recalls.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
-#from recalls.models import Recall
 
 from recalls.tasks import get_recalls
 
+
 class Command(BaseCommand):
-    help = "Imports recall data from csc"
+    help = "Imports recall data from digitalgov.gov"
 
     def handle(self, *args, **kwargs):
         get_recalls.apply_async()
+        self.stdout.write('Import complete.')
--- loop/loop/apps/recalls/tasks.py
+++ loop/loop/apps/recalls/tasks.py
@@ -1,13 +1,31 @@
 import datetime
+import logging
 import requests
 
 from celery import Celery
 
+from recalls.api_client import recall_api
+
 app = Celery()
 app.config_from_object('django.conf:settings')
 
-date = '2014-01-01'
+logger = logging.getLogger(__name__)
 
 @app.task
-def get_recalls(year=datetime.datetime.today().year):
-    print "test"
+def get_recalls(**kwargs):
+    """
+    A shim of a task to execute recall_api.get_recalls(), passes any kwargs
+    to the function.
+
+    returns the number of recalls that have been imported.
+    """
+
+    logger.info("Beginning recall import process.")
+
+    recalls = recall_api().get_recalls()
+
+    logger.info("Imported {num_recalls} recalls.".format(
+        num_recalls=len(recalls)
+    ))
+
+    return len(recalls)
