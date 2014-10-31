b46004ff6beacbb5ff79bee054cec4358a61de91
Justin Caratzas <bigjust@lambdaphil.es>
2014-10-31 13:39:40 -0400

    [SB-153] add sane date defaults for import command

--- loop/loop/apps/recalls/management/commands/import_recalls.py
+++ loop/loop/apps/recalls/management/commands/import_recalls.py
@@ -1,3 +1,6 @@
+import datetime
+
+from dateutil.relativedelta import *
 from optparse import make_option
 
 from django.core.management.base import BaseCommand, CommandError
@@ -12,10 +15,12 @@ class Command(BaseCommand):
         make_option('--start_date',
                     action='store',
                     dest='start_date',
+                    default=str(datetime.date.today() - relativedelta(months=1))
                 ),
         make_option('--end_date',
                     action='store',
                     dest='end_date',
+                    default=str(datetime.date.today())
                 ),
         make_option('--org',
                     action='store',
