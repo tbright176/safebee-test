215da2f0d9b5b06c556ab726f8202952d0013d5d
Justin Caratzas <bigjust@lambdaphil.es>
2014-11-04 07:32:17 -0500

    [SB-153] add links to recalls list view

--- loop/loop/apps/recalls/urls.py
+++ loop/loop/apps/recalls/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-from .views import (RecallHomePageView, RecallSearchView,
+from .views import (RecallHomePageView, RecallSearchView, RecallListView,
                     CarRecallDetailView, FoodRecallDetailView, ProductRecallDetailView)
 
 
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     url(r'cars/(?P<pk>\d+)/$', CarRecallDetailView.as_view(), name="car_recall_detail"),
     url(r'food/(?P<pk>\d+)/$', FoodRecallDetailView.as_view(), name="food_recall_detail"),
     url(r'products/(?P<pk>\d+)/$', ProductRecallDetailView.as_view(), name="product_recall_detail"),
+    url(r'all/$', RecallListView.as_view(), name="recalls_list"),
     url(r'search/$', RecallSearchView.as_view(), name="recalls_search"),
     url(r'^', RecallHomePageView.as_view(), name="recalls_home"),
 )
