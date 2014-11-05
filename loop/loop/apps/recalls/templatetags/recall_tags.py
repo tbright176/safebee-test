9196857cfca55ef8fbdbcca56149a41934720553
Justin Caratzas <bigjust@lambdaphil.es>
2014-11-05 15:12:27 -0500

    [SB-153] exclude food from alert&recalls widget

--- loop/loop/apps/recalls/templatetags/recall_tags.py
+++ loop/loop/apps/recalls/templatetags/recall_tags.py
@@ -1,11 +1,16 @@
 from django import template
+from django.contrib.contenttypes.models import ContentType
 
-from recalls.models import RecallStreamItem
+from recalls.models import RecallStreamItem, FoodRecall
 
 register = template.Library()
 
 
 @register.assignment_tag(takes_context=True)
 def latest_recalls(context, limit=3):
-    recalls = RecallStreamItem.objects.order_by('-recall_date', '-pk')[:limit]
+    """
+    Return `limit` number of recall items, ordered by most recent.
+    """
+    food_ct = ContentType.objects.get_for_model(FoodRecall)
+    recalls = RecallStreamItem.objects.exclude(content_type=food_ct).order_by('-recall_date', '-pk')[:limit]
     return recalls
