67734056a1cfe61d363e0a1adfb30e9308648a04
Ben Boyd <beathan@gmail.com>
2014-11-07 15:40:23 -0500

    Add templatetags for next/prev recall that does not really work b/c of DateField

--- loop/loop/apps/recalls/templatetags/recall_tags.py
+++ loop/loop/apps/recalls/templatetags/recall_tags.py
@@ -9,3 +9,25 @@ register = template.Library()
 def latest_recalls(context, limit=3):
     recalls = RecallStreamItem.objects.order_by('-recall_date', '-pk')[:limit]
     return recalls
+
+
+@register.assignment_tag
+def get_next_recall(current_recall):
+    next_item = None
+    try:
+        next_item = current_recall\
+            .get_next_by_recall_date()
+    except current_recall.__class__.DoesNotExist:
+        pass
+    return next_item
+
+
+@register.assignment_tag
+def get_previous_recall(current_recall):
+    previous_item = None
+    try:
+        previous_item = current_recall\
+            .get_previous_by_recall_date()
+    except current_recall.__class__.DoesNotExist:
+        pass
+    return previous_item
