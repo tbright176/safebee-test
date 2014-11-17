50f5674f39a8c94abcfe7697648b45be62c6d3f4
Justin Caratzas <bigjust@lambdaphil.es>
2014-11-17 12:34:30 -0500

    [SB-153] implement form validation for recall signup

--- loop/loop/apps/recalls/forms.py
+++ loop/loop/apps/recalls/forms.py
@@ -43,3 +43,32 @@ class RecallSignupForm(forms.Form):
     vehicle_year = forms.ChoiceField(choices=YEAR_CHOICES, required=False)
     vehicle_model = forms.ChoiceField(choices=MODEL_CHOICES, required=False)
     vehicle_make = forms.ModelChoiceField(queryset=CarMake.objects.all(), required=False)
+
+
+    def clean(self):
+        cleaned_data = super(RecallSignupForm, self).clean()
+
+        cat_checkboxes = ['products', 'vehicles', 'foodndrug']
+
+        if not cleaned_data.get('email_alerts') and not cleaned_data.get('phone_alerts'):
+            raise forms.ValidationError(
+                'You must select to recieve alerts via Email or SMS'
+            )
+
+        if not cleaned_data.get('email') and not cleaned_data.get('phone_number'):
+            if cleaned_data.get('email_alerts'):
+                raise forms.ValidationError(
+                    'Please enter a valid Email address.'
+                )
+            else:
+                raise forms.ValidationError(
+                    'Please enter a valid Phone Number.'
+                )
+
+        for checkbox in cat_checkboxes:
+            if cleaned_data.get(checkbox, False):
+                break
+
+            raise forms.ValidationError(
+                'You must select either Consumer Products, Motor Vehicles, or Food & Drug'
+            )
