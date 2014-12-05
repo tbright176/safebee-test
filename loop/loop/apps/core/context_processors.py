9a01239c4fc0a8bb0a7783f8fd8a6b26c3dfb892
Ben Boyd <beathan@gmail.com>
2014-12-05 09:47:34 -0500

    Add context processor for placing DEBUG in context

--- loop/loop/apps/core/context_processors.py
+++ loop/loop/apps/core/context_processors.py
@@ -1,5 +1,10 @@
+from django.conf import settings
 from django.contrib.sites.models import Site
 
 
 def site_processor(request):
     return {'current_site': Site.objects.get_current()}
+
+
+def debug_status(request):
+    return {'DEBUG': settings.DEBUG}
