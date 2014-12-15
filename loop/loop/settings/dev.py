fc0038dd7b4c51f19a5fa0644255008b60bd9932
Ben Boyd <beathan@gmail.com>
2014-12-15 14:23:46 -0500

    Remove prod DB settings from dev because Ben is an idiot

--- loop/loop/settings/dev.py
+++ loop/loop/settings/dev.py
@@ -1,18 +1,15 @@
 from .common import *
 
 
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-COMPRESS_ENABLED = True
+DEBUG = True
+TEMPLATE_DEBUG = True
+COMPRESS_ENABLED = False
 THUMBNAIL_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'safebee',
-        'USER': 'safebee',
-        'PASSWORD': 'mVeJPTirHeT9sxx*7m',
-        'HOST': 'safebeeprod1.cl21xbcn12v2.us-east-1.rds.amazonaws.com',
     },
 }
 
@@ -50,26 +47,26 @@ DEVSERVER_MODULES = (
 CACHE_CONTROL_MAX_AGE = 0
 
 # django-storages
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'loop.storage.S3StaticStorage'
-THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'media.dev.safebee.com'
-AWS_STATIC_BUCKET_NAME = 'static.dev.safebee.com'
-AWS_HEADERS = {
-    'Expires': 'Fri, 31 Dec 2031 23:59:59 GMT',
-    'Cache-Control': 'max-age=86400',
-}
-AWS_IS_GZIPPED = True
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#STATICFILES_STORAGE = 'loop.storage.S3StaticStorage'
+#THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#AWS_STORAGE_BUCKET_NAME = 'media.dev.safebee.com'
+#AWS_STATIC_BUCKET_NAME = 'static.dev.safebee.com'
+#AWS_HEADERS = {
+#    'Expires': 'Fri, 31 Dec 2031 23:59:59 GMT',
+#    'Cache-Control': 'max-age=86400',
+#}
+#AWS_IS_GZIPPED = True
 
 # S3-only keys
 AWS_ACCESS_KEY_ID = "AKIAIS6QKO33FI26PXWA"
 AWS_SECRET_ACCESS_KEY = "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"
 
-AWS_S3_SECURE_URLS = False
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
-AWS_STATIC_CUSTOM_DOMAIN = AWS_STATIC_BUCKET_NAME
-STATIC_URL = 'http://%s/' % AWS_STATIC_BUCKET_NAME
-MEDIA_URL = 'http://%s/' % AWS_STORAGE_BUCKET_NAME
+#AWS_S3_SECURE_URLS = False
+#AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
+#AWS_STATIC_CUSTOM_DOMAIN = AWS_STATIC_BUCKET_NAME
+#STATIC_URL = 'http://%s/' % AWS_STATIC_BUCKET_NAME
+#MEDIA_URL = 'http://%s/' % AWS_STORAGE_BUCKET_NAME
 
 
 # Social Settings
