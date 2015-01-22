777e99d8a5fdfce7ef01ac7876315b96934d4911
Ben Boyd <beathan@gmail.com>
2015-01-22 14:34:31 -0500

    Add media:content element to RSS feeds with promo image, resolves #410

--- loop/loop/apps/core/feeds.py
+++ loop/loop/apps/core/feeds.py
@@ -5,12 +5,54 @@ from django.core.urlresolvers import reverse
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.utils.feedgenerator import Rss201rev2Feed
 from django.utils.text import slugify
+
+from easy_thumbnails.exceptions import InvalidImageFormatError
+from easy_thumbnails.files import get_thumbnailer
 
 from .models import Article, Slideshow, StreamItem, Category, Tag, LoopUser
 
 
-class LatestContentFeed(Feed):
+class ExtendedRSSFeed(Rss201rev2Feed):
+    """
+    Extends RSS feed to add media:content element.
+    """
+    def rss_attributes(self):
+        attrs = super(ExtendedRSSFeed, self).rss_attributes()
+        attrs['xmlns:media'] = 'http://search.yahoo.com/mrss/'
+        return attrs
+
+    def add_item_elements(self, handler, item):
+        super(ExtendedRSSFeed, self).add_item_elements(handler, item)
+        if 'media_content' in item:
+            handler.addQuickElement(u'media:content', '',
+                                    item['media_content'])
+
+
+class LoopContentFeed(Feed):
+    feed_type = ExtendedRSSFeed
+
+    def item_extra_kwargs(self, item):
+        extra = {}
+        try:
+            image = item.promo_image.asset
+            image = get_thumbnailer(image)\
+                .get_thumbnail({'size': (600, 400),
+                                'crop': 'smart',
+                                'quality': 65})
+            extra = {'media_content':\
+                     {'url': image.url,
+                      'height': '%s' % image.height,
+                      'width': '%s' % image.width,
+                      'fileSize': '%s' % image.size,
+                      'type': 'image/jpeg'}}
+        except InvalidImageFormatError:
+            pass
+        return extra
+
+
+class LatestContentFeed(LoopContentFeed):
     link = "/feeds/latest/"
 
     def title(self, obj):
@@ -20,7 +62,7 @@ class LatestContentFeed(Feed):
         return StreamItem.rss.all()[:settings.CORE_DEFAULT_FEED_LENGTH]
 
 
-class CategoryFeed(Feed):
+class CategoryFeed(LoopContentFeed):
     """
     A feed of all StreamItems belonging to the specified category.
     """
@@ -39,7 +81,7 @@ class CategoryFeed(Feed):
         return get_object_or_404(Category, slug=slug)
 
 
-class TagFeed(Feed):
+class TagFeed(LoopContentFeed):
     """
     A feed of all StreamItems belonging to the specified tag.
     """
@@ -57,7 +99,7 @@ class TagFeed(Feed):
         return get_object_or_404(Tag, slug=slug)
 
 
-class AuthorFeed(Feed):
+class AuthorFeed(LoopContentFeed):
 
     def get_object(self, request, basename):
         basename = slugify(basename)
