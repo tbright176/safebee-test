94441e433c90b4686a3ed089fbaa852b78a53d95
Ben Boyd <beathan@gmail.com>
2015-01-27 11:58:41 -0500

    Adds desktop NL signup, refs #328

---
+++ loop/loop/static/js/app.js
@@ -0,0 +1,46 @@
+$.modal.defaults['showClose'] = false;
+
+function setModalCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000 * 365));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString() + ';path=/';
+}
+
+function getModalCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
+
+function mcSignupCallback(resp) {
+    if (resp.result === 'success') {        
+        var submit = $("#nl_signup_modal_submit");
+        submit.attr("disabled", true);
+        submit.css({"background-color": "#b6541e", "color": "#fff"});
+        submit.text("Thanks, we'll be in touch!");
+        setTimeout(function() {
+            $.modal.close();
+        }, 2000)
+    }
+}
+
+$(document).ready(function() {
+    // check cookie
+    var visited = getModalCookie("visited");
+
+    if (visited == null) {
+        setModalCookie('visited', 1);
+    }
+    else {
+        visited = parseInt(visited);
+        // if (visited === 1) { // 2nd pageview
+        if (visited >= 0) {
+            $('#nl_signup_modal').ajaxChimp({
+                url: 'http://mnn.us4.list-manage.com/subscribe/post?u=6df70d8dcc50e45d16f196d8c&amp;id=6e238acf12',
+                callback: mcSignupCallback,
+            });
+            $('#nl_signup_modal').modal();
+            ga('send', 'event', 'Newsletter Popup', 'Modal Displayed');
+        }
+        setModalCookie('visited', visited + 1);
+    }
+});
