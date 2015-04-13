5dad17463656284c87cd587e010d5c868cc34a85
Nick Incardone <nickincardone@gmail.com>
2015-04-13 14:37:19 -0400

    final touches refs #546

--- loop/loop/static/js/app.js
+++ loop/loop/static/js/app.js
@@ -43,15 +43,16 @@ $(document).ready(function() {
         }
         setModalCookie('visited', visited + 1);
     }
-    if ($('.article-body').offset()) {
-        sb_article_bottom = $('.article-body').offset().top + ($('.article-body').height() * .6);
-    } else {
-        sb_article_bottom = 99999;
-    }
     if ($('.sb-resize-9').offset()) {
-        sb_final_static = $('.sb-resize-9').offset().top + ($('.sb-resize-9').height() * .5);
+        sb_final_static = $('.sb-resize-9').offset().top + ($('.sb-resize-9').height() * .3);
     } else {
         sb_final_static = 99999;
+    }
+    if ($('.article-body').offset()) {
+        sb_article_bottom = $('.article-body').offset().top + ($('.article-body').height() * .6);
+        sb_final_static = 99999;
+    } else {
+        sb_article_bottom = 99999;
     }
     $(window).on("mousewheel DOMMouseScroll MozMousePixelScroll", function(e) {
         if ($(window).scrollTop() > sb_article_bottom || $(window).scrollTop() > sb_final_static) {
