89ec8742c3fd6b45ded461db56201953e8c13b15
Justin Caratzas <bigjust@lambdaphil.es>
2015-11-09 19:06:24 -0500

    implement haystack ES indexing

--- loop/loop/settings/local.py
+++ loop/loop/settings/local.py
@@ -23,8 +23,6 @@ CACHES = {
 
 INSTALLED_APPS += (
     'debug_toolbar.apps.DebugToolbarConfig',
-    'django_nose',
-    'kombu.transport.django',
 )
 
 ALLOWED_HOSTS = ['*',]
@@ -42,9 +40,6 @@ TWITTER_API_SECRET_KEY = ''
 TWITTER_OAUTH_ACCESS_TOKEN = ''
 TWITTER_OAUTH_SECRET_TOKEN = ''
 BITLY_CUSTOM_DOMAIN = ''
-
-# Celery
-BROKER_URL = 'django://'
 
 AWS_ACCESS_KEY_ID = "AKIAIS6QKO33FI26PXWA"
 AWS_SECRET_ACCESS_KEY = "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"
